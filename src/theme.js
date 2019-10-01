e18d06ce1592f5bacd3c08596bab2373d3593769
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-10-01 15:26:40 -0700

    set up C5 theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export default {
+  googleFont:
+    "https://fonts.googleapis.com/css?family=Fira+Code|Slabo+27px",
+  fonts: {
+    body: '"Slabo 27px", system-ui, sans-serif',
+    monospace: '"Fira Code", monospace'
+  },
+  colors: {
+    text: "#fff",
+    background: "#404040",
+    primary: "#025A6E",
+    secondary: "#EF8555",
+    black: "#000"
+  }
+};
